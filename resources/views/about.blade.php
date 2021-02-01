ab93c17a875f5f5e61d5872bf7ef812e30a549df
ihza rizky <ihzarizky30@gmail.com>
2021-02-02 06:03:30 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About - Belajar Laravel</title>
+</head>
+<body>
+
+	<h1>
+		About - Belajar Laravel 2021
+	</h1>
+
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home - Belajar Laravel</title>
+</head>
+<body>
+
+	<h1>
+		Home - Belajar Laravel 2021
+	</h1>
+
+</body>
+</html>
